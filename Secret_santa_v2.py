544bf713e9a571ee41973fe7c864e99eb79f8b58
steph-rage <stephlage621@gmail.com>
2016-12-06 13:58:22 -0500

    Now gets values from a Google Sheets document and uses them to assign santas. Still needed: support for restrictions.

--- Secret_santa_v2.py
+++ Secret_santa_v2.py
@@ -2,11 +2,44 @@ from random import randint
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from Santa_Values import participants
 
 
-participants = {"Person": "email"}
-couples = []
+'''In order to use this tool, please fist create a spreadsheet in google drive. The spreadshet will need to have three columns: Name, Email or Phone Number, and Restrictions. Please choose if you would like to contact your participants by sending a personal email or a text message, and title your column accordingly. Once your participants have all filled out their information in your spreadsheet, you are ready to run the code!'''
+
+restrictions = []
+for person in participants:
+	restrictions.append(person)
 worked = False
+
+def send_email():
+	fromaddr = "me@example.com"
+	gmail_password = "password"
+
+	for email, giftee in santa_matches.items():
+		toaddr = email
+		msg = MIMEMultipart()
+		msg['From'] = fromaddr
+		msg['To'] = toaddr
+		msg['Subject'] = "Secret Santa"
+		 
+		body = "Personal message: Here's your person to buy for: {}".format(giftee)
+		msg.attach(MIMEText(body, 'plain'))
+
+		message = msg.as_string()
+
+		
+		s = smtplib.SMTP('smtp.gmail.com', 587)
+		s.ehlo()
+		s.starttls()
+		s.ehlo()
+		s.login(fromaddr, gmail_password)
+		s.sendmail(fromaddr, toaddr, message)
+		s.close()
+		print('email sent to {}'.format(email))
+		
+#def send_text():
+
 
 
 while worked == False:
@@ -16,7 +49,7 @@ while worked == False:
 		choose_from.append(person)
 	for person, email in participants.items():
 		person_choices = choose_from.copy()
-		for pair in couples:
+		for pair in restrictions:
 			if person in pair:
 				for i in range(2):
 					try:
@@ -33,28 +66,4 @@ while worked == False:
 	if len(santa_matches) == len(participants):
 		worked = True
 
-fromaddr = "me@example.com"
-gmail_password = "password"
-
-for email, giftee in santa_matches.items():
-	toaddr = email
-	msg = MIMEMultipart()
-	msg['From'] = fromaddr
-	msg['To'] = toaddr
-	msg['Subject'] = "Mansion on Mansion Secret Santa"
-	 
-	body = "Personal message: Here's your person to buy for: {}".format(giftee)
-	msg.attach(MIMEText(body, 'plain'))
-
-	message = msg.as_string()
-
-	
-	s = smtplib.SMTP('smtp.gmail.com', 587)
-	s.ehlo()
-	s.starttls()
-	s.ehlo()
-	s.login(fromaddr, gmail_password)
-	s.sendmail(fromaddr, toaddr, message)
-	s.close()
-	print('email sent to {}'.format(email))
-	
+print(santa_matches)
